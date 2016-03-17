665532be91d52c26464fdc3fb59508a67e31f194
Li Xuanji <xuanji@quizlet.com>
2016-03-17 00:33:14 -0700

    D

--- codeforces/651/D/D.cpp
+++ codeforces/651/D/D.cpp
@@ -9,32 +9,37 @@ typedef long long ll;
 ll N, a, b, T;
 string S;
 
-ll numSwipe(int start, int end) {
-  // cost to swipe the interval [start, end]
+const int MAX_N = 200000;
 
-  if (start == end) return 0;
-  if (start < end) return (end - start);
-  if (start > end) return numSwipe(start, N) + numSwipe(0, end);
-  return -1;
+// number of w photos in [0, i)
+int numWEnding[MAX_N+1]; 
+
+int numW(int start, int end) {
+  // num of w photos in the circular interval [start, end]
+  if (start <= end) return numWEnding[end+1] - numWEnding[start];
+  return numW(start, N-1) + numW(0, end);
 }
 
-int costView(int start, int end) {
+int numSwipes(int start, int end) {
+  // of swipes in the circular interval [start, end]
+  if (start <= end) return end - start;
+  return numSwipes(start, N) + numSwipes(0, end);
+}
 
-  // cost to view the interval [start, end]
+ll costView(int start, int end) {
 
-  ll cv = 0;
+  // cost to view the circular interval [start, end]
+  int numPhotos = numSwipes(start, end) + 1;
+  return numPhotos + b * numW(start, end);
 
-  int i = start;
+}
 
-  while (1) {
-    cv += 1;
-    if (S[i] == 'w') cv += b;
-    if (i == end) break;
-    i = (i + 1) % N;
-  }
+int costUse(int start, int end) {
 
-  ll leftSwipes = numSwipe(start, 0);
-  ll rightSwipes = numSwipe(0, end);
+  // cost to use the interval [start, end] starting at element 0
+
+  ll leftSwipes = numSwipes(start, 0);
+  ll rightSwipes = numSwipes(0, end);
 
   ll numSwipes = 0;
 
@@ -44,22 +49,31 @@ int costView(int start, int end) {
     numSwipes = 2 * rightSwipes + leftSwipes;
   }
 
-  return a*numSwipes + cv;
+  return a*numSwipes + costView(start, end);
 }
 
 int main() {
   cin >> N >> a >> b >> T;
   getline(cin, S);
   getline(cin, S);
-  cout << S << endl;
+  // cout << S << endl;
 
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
-      if (0 < i && i <= j) continue;
-      if (costView(i, j) <= T) {
-        cout << "(" << i << " " << j << ")" << " " << numSwipe(i, j) + 1 << " " << costView(i, j) << endl;
-      }
-    }
+  // fill up numWEnding
+  numWEnding[0] = 0;
+  for (int i=1; i<=N; i++) {
+    numWEnding[i] = numWEnding[i-1];
+    if (S[i-1] == 'w') numWEnding[i] += 1;
   }
+
+  cout << numW(7, 4) << endl;
+
+  // for (int i = 0; i < N; i++) {
+  //   for (int j = 0; j < N; j++) {
+  //     if (0 < i && i <= j) continue;
+  //     if (costUse(i, j) <= T) {
+  //       cout << "(" << i << " " << j << ")" << " " << numSwipes(i, j) + 1 << " " << costUse(i, j) << endl;
+  //     }
+  //   }
+  // }
 
 }
