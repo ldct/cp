36fc99faad8226e0f9c2ce9e0d617b6255c4a1c0
Xuanji Li <xuanji@gmail.com>
2020-05-19 22:47:52 +0800

    641

--- codeforces/641/A/A.cpp
+++ codeforces/641/A/A.cpp
@@ -10,11 +10,14 @@ int gcd(int a, int b) {
   return gcd(b, a % b);
 }
 
-int lcm(int a, int b) {
-  return (a*b) / gcd(a, b);
+unsigned long long lcm(unsigned long long a, unsigned long long b) {
+  return (a / gcd(a, b)) * b;
 }
 
 int a[MAX_N];
+int prefix[MAX_N];
+int suffix[MAX_N];
+int rest[MAX_N];
 
 int main() {
   
@@ -26,13 +29,44 @@ int main() {
     scanf("%d", &a[i]);
   }
 
-
-  int g = a[0];
-  for (int i=0; i<N; i++) {
-    g = gcd(g, a[i]);
+  prefix[0] = a[0];
+  for (int i=1; i<N; i++) {
+    prefix[i] = gcd(prefix[i-1], a[i]);
   }
 
-  printf("%d\n", g);
-  
+  suffix[N-1] = a[N-1];
+  for (int i=N-1; i>=0; i--) {
+    suffix[i] = gcd(suffix[i+1], a[i]);    
+  }
+
+  rest[0] = suffix[1];
+  rest[N-1] = prefix[N-2];
+
+  for (int i=1; i<N-1; i++) {
+    rest[i] = gcd(prefix[i-1], suffix[i+1]);
+  }
+
+  // for (int i=0; i<N; i++) {
+  //   printf("%d ", prefix[i]);
+  // }
+  // printf("\n");
+
+  // for (int i=0; i<N; i++) {
+  //   printf("%d ", suffix[i]);
+  // }
+  // printf("\n");
+
+  // for (int i=0; i<N; i++) {
+  //   printf("%d ", rest[i]);
+  // }
+  // printf("\n");
+
+  unsigned long long ans = 1;
+  for (int i=0; i<N; i++) {
+    ans = lcm(ans, rest[i]);
+  }
+
+  printf("%llu\n", ans);
+
   return 0;
 }
