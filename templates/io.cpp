e64578e778727e63fafb5061fde5259c5fb98630
Xuanji Li <xuanji@gmail.com>
2020-07-07 14:16:32 +0800

    c++ io

---
+++ templates/io.cpp
@@ -0,0 +1,11 @@
+template<class T> ostream& operator << (ostream& os, const vector<T>& v) { os << "["; for (int i=0; i<v.size(); i++) { os << v[i]; if (i < v.size() - 1) os << ", "; } return os << "]"; }
+
+
+/*
+template<class T>
+ostream& operator << (ostream& os, const vector<T>& v) {
+    os << "[";
+    for (int i=0; i<v.size(); i++) { os << v[i]; if (i < v.size() - 1) os << ", "; }
+    return os << "]";
+}
+*/
