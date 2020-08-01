2f104433b45b862faed8555893637e96ba17271e
Xuanji Li <xuanji@gmail.com>
2020-08-02 07:22:33 +0800

    clean up ring_n

--- templates/numeric_t/ring_n.cpp
+++ templates/numeric_t/ring_n.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// standard representitives in the ring of integers modulo N
+// integers with addition modulo N
 
 constexpr int MODULUS = 1000000007;
 
@@ -9,11 +9,12 @@ template<typename numeric_t = long long, numeric_t MODULUS = MODULUS>
 class RInt {
 public:
   numeric_t val;
-  RInt (numeric_t _val) { val = _val; }
+  RInt (numeric_t _val) { val = ((_val % MODULUS) + MODULUS) % MODULUS; }
   friend ostream& operator << (ostream& os, const RInt& s) { return os << s.val; }
-  RInt operator+ (const RInt& r) const { return RInt((this->val + r.val) % MODULUS); }
-  RInt operator* (const RInt& r) const { return RInt((this->val * r.val) % MODULUS); }
+  RInt operator+ (const RInt& r) const { return RInt(this->val + r.val); }
+  RInt operator* (const RInt& r) const { return RInt(this->val * r.val); }
   RInt& operator+= (const RInt& r) { this->val += r.val; this->val %= MODULUS; return *this; }
+  RInt& operator*= (const RInt& r) { this->val *= r.val; this->val %= MODULUS; return *this; }
 };
 
 int main() { 
