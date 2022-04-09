4d348a446298b44723c5e9a502585f1d651eb81a
Li Xuanji <xuanji@gmail.com>
2022-04-09 09:15:53 +0800

    abc

---
+++ abc240/A/A.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env pypy3
+
+import io, os, sys
+from sys import stdin, stdout
+
+# input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
+def input(): return stdin.readline().strip()
+def read_int_list(): return list(map(int, input().split()))
+def read_int_tuple(): return tuple(map(int, input().split()))
+def read_int(): return int(input())
+
+from itertools import permutations, chain, combinations, product
+from math import factorial, gcd
+from collections import Counter, defaultdict, deque
+from heapq import heappush, heappop, heapify
+from bisect import bisect_left
+from functools import lru_cache
+
+### CODE HERE
+
+a, b = read_int_tuple()
+a %= 10
+b %= 10
+print("Yes" if abs(a - b) in [1, 9] else "No")
